fb1635183613227d315db76c497f72e435b6bd10
madclaws <pavanan.anandu63@gmail.com>
2025-03-01 23:22:23 +0530

    feat: added get, put and find_comp functions

--- lib/kademlia/node.ex
+++ lib/kademlia/node.ex
@@ -1,11 +1,19 @@
 defmodule Kademlia.Node do
   @moduledoc """
-  Represents a real-word computer/node
+  Represents a real-world computer/node
+
+  how keys can be mapped to 6-bit space?
+
+  key will be hashed to 6-bit space..., for now lets keys be a number which is explicitly inside 6-bit space while saving..
+
   """
 
-  @default_k 2
+  @default_k 4
   @default_bitspace 6
   @default_a 3
+  # 1 min
+  @timeout :timer.seconds(60)
+
   use GenServer
   require Logger
 
@@ -16,6 +24,18 @@ defmodule Kademlia.Node do
     GenServer.start_link(__MODULE__, args, [])
   end
 
+  def get(pid, key) do
+    GenServer.call(pid, {:get, key}, @timeout)
+  end
+
+  def put(pid, key, val) do
+    GenServer.call(pid, {:put, key, val}, @timeout)
+  end
+
+  def find_comp(pid, id) do
+    GenServer.call(pid, {:find_comp, id}, @timeout)
+  end
+
   def get_id(pid) do
     :sys.get_state(pid).info |> elem(1)
   end
@@ -24,8 +44,59 @@ defmodule Kademlia.Node do
     :sys.get_state(pid).routing_table
   end
 
+  ################
   def init(args) do
     {:ok, create_initial_state(args)}
+  end
+
+  def handle_call({:get, key}, {_caller_pid, _}, state) do
+    Logger.info("NODE #{elem(state.info, 1)}:: GET operation for KEY #{key}")
+    current_bitspace = Application.get_env(:kademlia, :bitspace, @default_bitspace)
+
+    cond do
+      not is_number(key) ->
+        {:reply, {:error, "Key should be number"}, state}
+
+      key > Utils.get_max_id(current_bitspace) ->
+        {:reply, {:error, "Key is out of bitspace (#{current_bitspace})"}, state}
+
+      true ->
+        {:reply, state.hash_map[key], state}
+    end
+  end
+
+  def handle_call({:put, key, val}, {_caller_pid, _}, state) do
+    Logger.info("NODE #{elem(state.info, 1)}:: PUT operation for KEY #{key} VAL #{val}")
+    current_bitspace = Application.get_env(:kademlia, :bitspace, @default_bitspace)
+
+    if key > current_bitspace do
+    end
+
+    cond do
+      not is_number(key) ->
+        {:reply, {:error, "Key should be number"}, state}
+
+      key > Utils.get_max_id(current_bitspace) ->
+        {:reply, {:error, "Key is out of bitspace (#{current_bitspace})"}, state}
+
+      true ->
+        hash_map = Map.put(state.hash_map, key, val)
+        state = Map.put(state, :hash_map, hash_map)
+        {:reply, {:ok, val}, state}
+    end
+  end
+
+  def handle_call({:find_comp, id}, _from, state) do
+    # finding the closest nodes to id from the routing table
+    closest_ndoes =
+      Map.values(state.routing_table)
+      |> List.flatten()
+      |> Enum.sort_by(fn {node_id, _} ->
+        Bitwise.bxor(node_id, id)
+      end)
+      |> Enum.slice(0..state.k)
+
+    {:reply, closest_ndoes, state}
   end
 
   @spec create_initial_state(Keyword.t()) :: map()
@@ -44,7 +115,7 @@ defmodule Kademlia.Node do
     )
 
     routing_table = Utils.create_routing_table(node_id, bitspace)
-    Logger.info("Routing table\n #{inspect(routing_table)}")
+    Logger.info("Node #{node_id}:: Routing table\n #{inspect(routing_table)}")
 
     %{
       k: Application.get_env(:kademlia, :k, @default_k),
--- test/kademlia_test.exs
+++ test/kademlia_test.exs
@@ -1,8 +1,23 @@
 defmodule KademliaTest do
   use ExUnit.Case
   doctest Kademlia
+  @k 2
 
-  test "greets the world" do
-    assert Kademlia.hello() == :world
+  @tag :clos
+  test "closest k nodes from the routing table" do
+    table = %{
+      "00" => [{0, "adad"}],
+      "010" => [{2, "adad"}],
+      "1" => [{7, "adad"}, {6, "adad"}]
+    }
+
+    id = 3
+
+    Map.values(table)
+    |> List.flatten()
+    |> Enum.sort_by(fn {a, _} ->
+      Bitwise.bxor(a, id)
+    end)
+    |> IO.inspect()
   end
 end
